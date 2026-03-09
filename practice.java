61e8f83fed46ea130883d334d531cc2a456124ca
Prashant Saud <saudprashant01@gmail.com>
2026-03-09 16:19:31 +0545

    commited all files once

---
+++ practice.java
@@ -0,0 +1,7 @@
+public class practice {
+    public static void main(String[] args) throws Exception {
+        System.out.println("loading.........");
+        Thread.sleep(2000);
+        System.out.print("\r done");
+    }
+}
